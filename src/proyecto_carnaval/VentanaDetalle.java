f8c4ae075f588119433d48e22928c972c87e8a60
DAW <DAW@Jesus>
2013-05-14 20:54:32 +0200

    Commit

--- src/proyecto_carnaval/VentanaDetalle.java
+++ src/proyecto_carnaval/VentanaDetalle.java
@@ -14,38 +14,38 @@ import javax.swing.DefaultListModel;
  * @author DAW
  */
 public class VentanaDetalle extends javax.swing.JDialog {
-
+    
     private DatosAgrupaciones agrupacion;
     boolean aceptado;
     private GestionAgrupaciones gestion = new GestionAgrupaciones();
-
+    
     public VentanaDetalle(java.awt.Frame parent, boolean modal) {
-
-
+        
+        
         super(parent, modal);
         initComponents();
         this.CargarDatosCombo();
-
-
-
+        
+        
+        
     }
-
+    
     public DatosAgrupaciones getAgrupacion() {
         return agrupacion;
     }
-
+    
     private void CargarDatosCombo() {
         ArrayList listaNiveles = gestion.findModalidad();
-
+        
         int numElementos = listaNiveles.size();
         String[] arrayParaJComboBox = new String[numElementos];
         listaNiveles.toArray(arrayParaJComboBox);
         ComboBoxModel c = new DefaultComboBoxModel(arrayParaJComboBox);
         jComboBox1.setModel(c);
-
-
+        
+        
     }
-
+    
     public void setAgrupacion(DatosAgrupaciones agrupacion) {
         this.agrupacion = agrupacion;
         if (agrupacion.getId() != 0) {
@@ -62,7 +62,7 @@ public class VentanaDetalle extends javax.swing.JDialog {
         localidad.setText(agrupacion.getLocalidad());
         imagen.setText(String.valueOf(agrupacion.getImagenAgrupacion()));
     }
-
+    
     private void aceptar() {
         aceptado = true;
         agrupacion.setNombre(nombre.getText());
@@ -74,18 +74,19 @@ public class VentanaDetalle extends javax.swing.JDialog {
         agrupacion.setLocalidad(localidad.getText());
         agrupacion.setImagenAgrupacion(null);
         this.setVisible(false);
-
+        
     }
-
+    
     public boolean aceptado() {
         return aceptado;
-
+        
     }
-
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jComboBox2 = new javax.swing.JComboBox();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
@@ -101,12 +102,14 @@ public class VentanaDetalle extends javax.swing.JDialog {
         director = new javax.swing.JTextField();
         localidad = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
-        jToggleButton1 = new javax.swing.JToggleButton();
         jLabel8 = new javax.swing.JLabel();
         id = new javax.swing.JLabel();
         jLabel9 = new javax.swing.JLabel();
         imagen = new javax.swing.JTextField();
         jComboBox1 = new javax.swing.JComboBox();
+        jButton2 = new javax.swing.JButton();
+
+        jComboBox2.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -133,13 +136,18 @@ public class VentanaDetalle extends javax.swing.JDialog {
             }
         });
 
-        jToggleButton1.setText("Cancelar");
-
         jLabel8.setText("Identificador");
 
         jLabel9.setText("Imagen");
 
         jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        jButton2.setText("Cancelar");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -168,7 +176,7 @@ public class VentanaDetalle extends javax.swing.JDialog {
                                     .addComponent(autorLetra, javax.swing.GroupLayout.PREFERRED_SIZE, 241, javax.swing.GroupLayout.PREFERRED_SIZE)
                                     .addComponent(autorMusica)
                                     .addComponent(nombre))
-                                .addContainerGap(15, Short.MAX_VALUE))
+                                .addContainerGap(20, Short.MAX_VALUE))
                             .addGroup(jPanel1Layout.createSequentialGroup()
                                 .addComponent(id, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
@@ -176,16 +184,15 @@ public class VentanaDetalle extends javax.swing.JDialog {
                                 .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jButton1)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(jToggleButton1))
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(jLabel9)
-                                .addGap(58, 58, 58)
-                                .addComponent(imagen, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addContainerGap())))
+                        .addComponent(jLabel9)
+                        .addGap(58, 58, 58)
+                        .addComponent(imagen, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap())
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jButton1)
+                        .addGap(26, 26, 26)
+                        .addComponent(jButton2)
+                        .addGap(153, 153, 153))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -232,8 +239,8 @@ public class VentanaDetalle extends javax.swing.JDialog {
                     .addComponent(imagen, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 86, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jToggleButton1)
-                    .addComponent(jButton1)))
+                    .addComponent(jButton1)
+                    .addComponent(jButton2)))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -245,7 +252,7 @@ public class VentanaDetalle extends javax.swing.JDialog {
                 .addGroup(layout.createSequentialGroup()
                     .addGap(32, 32, 32)
                     .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addContainerGap(39, Short.MAX_VALUE)))
+                    .addContainerGap(34, Short.MAX_VALUE)))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -264,8 +271,12 @@ public class VentanaDetalle extends javax.swing.JDialog {
         aceptado = true;
         this.aceptar();
         this.setVisible(false);
-
+        
     }//GEN-LAST:event_jButton1ActionPerformed
+    
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        this.setVisible(false);
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -316,7 +327,9 @@ public class VentanaDetalle extends javax.swing.JDialog {
     private javax.swing.JLabel id;
     private javax.swing.JTextField imagen;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JComboBox jComboBox1;
+    private javax.swing.JComboBox jComboBox2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -327,7 +340,6 @@ public class VentanaDetalle extends javax.swing.JDialog {
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JToggleButton jToggleButton1;
     private javax.swing.JTextField localidad;
     private javax.swing.JTextField nombre;
     // End of variables declaration//GEN-END:variables
